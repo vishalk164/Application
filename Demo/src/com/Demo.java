f4fa9c200f9c1a93c59903e8caaf657ea27a066b
VISHAL SUBHASH KAMBLE <vishalkamble375@gmail.com>
2023-01-27 19:28:28 +0530

    secondCode

--- Demo/src/com/Demo.java
+++ Demo/src/com/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("This Is My First Code For Git");
+		System.out.println("---------------");
 	}
 
 }
---
+++ Demo/src/com/Demo2.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("My secound code for git");
+	}
+
+}
